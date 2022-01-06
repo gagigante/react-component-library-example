6251c73ec3af74ab23a856bfd318c62b1afc2358
Gabriel Gigante <gabriel.silva@luizalabs.com>
2022-01-06 09:57:20 -0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+
+      <Button label="Teste" />
+    </div>
+  )
+}
+
+export default App
